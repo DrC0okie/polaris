58a83cada4dcaf350ca61eaa5adbf14cae46e81a
DrCookie <timothee.vanhove@heig-vd.ch>
2025-05-17 09:30:38 +0200

    fix: counter-sign mechanism

--- polaris-beacon/src/ble/beacon_advertiser.cpp
+++ polaris-beacon/src/ble/beacon_advertiser.cpp
@@ -8,9 +8,9 @@
 // Defined in ble_server.h or a common header
 const uint8_t EXTENDED_BROADCAST_ADV_INSTANCE = 1;
 
-BeaconAdvertiser::BeaconAdvertiser(uint32_t beacon_id, const uint8_t sk[Ed25519_SK_SIZE],
+BeaconAdvertiser::BeaconAdvertiser(uint32_t beaconId, const uint8_t sk[Ed25519_SK_SIZE],
                                    MinuteCounter& counter, BLEMultiAdvertising& advertiser)
-    : _beacon_id(beacon_id), _sk(sk), _counterRef(counter), _advertiserRef(advertiser) {
+    : _beaconId(beaconId), _sk(sk), _counterRef(counter), _advertiserRef(advertiser) {
 }
 
 void BeaconAdvertiser::begin() {
@@ -33,37 +33,36 @@ void BeaconAdvertiser::handleCounterIncrement() {
 }
 
 void BeaconAdvertiser::updateAdvertisement() {
-    uint64_t current_counter = _counterRef.getValue();
-    BroadcastPayload payload_content;
-    payload_content.beacon_id = _beacon_id;
-    payload_content.counter = current_counter;
+    uint64_t currentCounter = _counterRef.getValue();
+    BroadcastPayload payloadContent;
+    payloadContent.beaconId = _beaconId;
+    payloadContent.counter = currentCounter;
 
-    // Sign beacon_id and counter
-    signBeaconBroadcast(payload_content.signature, _beacon_id, current_counter, _sk);
+    // Sign beaconId and counter
+    signBeaconBroadcast(payloadContent.signature, _beaconId, currentCounter, _sk);
 
     // Construct the actual advertising data (e.g., Manufacturer Specific Data)
     // Format: [Len1][Type1][ManufID_LSB][ManufID_MSB][BroadcastPayload_Bytes]
-    const uint16_t manufacturer_id = 0xABCD;                             // company ID
-    const size_t data_len_for_adv = 2 + BroadcastPayload::packedSize();  // ManufID + Payload
+    const uint16_t manufacturerId = 0xABCD;                           // company ID
+    const size_t dataLenForAdv = 2 + BroadcastPayload::packedSize();  // ManufID + Payload
 
-    uint8_t raw_adv_payload[1 + 1 + data_len_for_adv];  // Full AD Structure: Len + Type + Data
+    uint8_t rawAdvPayload[1 + 1 + dataLenForAdv];  // Full AD Structure: Len + Type + Data
     size_t idx = 0;
 
-    raw_adv_payload[idx++] =
-        1 + data_len_for_adv;  // Length of this AD structure field (Type + Data)
-    raw_adv_payload[idx++] = ESP_BLE_AD_MANUFACTURER_SPECIFIC_TYPE;  // Type
-    raw_adv_payload[idx++] = (uint8_t)(manufacturer_id & 0xFF);      // Manuf ID LSB
-    raw_adv_payload[idx++] = (uint8_t)(manufacturer_id >> 8);        // Manuf ID MSB
+    rawAdvPayload[idx++] = 1 + dataLenForAdv;  // Length of this AD structure field (Type + Data)
+    rawAdvPayload[idx++] = ESP_BLE_AD_MANUFACTURER_SPECIFIC_TYPE;  // Type
+    rawAdvPayload[idx++] = (uint8_t)(manufacturerId & 0xFF);       // Manuf ID LSB
+    rawAdvPayload[idx++] = (uint8_t)(manufacturerId >> 8);         // Manuf ID MSB
 
     // Copy serialized payload content
-    memcpy(&raw_adv_payload[idx], &payload_content, BroadcastPayload::packedSize());
+    memcpy(&rawAdvPayload[idx], &payloadContent, BroadcastPayload::packedSize());
     idx += BroadcastPayload::packedSize();
 
     // Update the advertising data for the extended instance
-    if (!_advertiserRef.setAdvertisingData(EXTENDED_BROADCAST_ADV_INSTANCE, idx, raw_adv_payload)) {
+    if (!_advertiserRef.setAdvertisingData(EXTENDED_BROADCAST_ADV_INSTANCE, idx, rawAdvPayload)) {
         Serial.println("[BeaconAdv] Failed to update extended advertising data.");
     } else {
         Serial.printf("[BeaconAdv] Extended advertisement updated. Counter: %llu\n",
-                      current_counter);
+                      currentCounter);
     }
 }
