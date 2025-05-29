5f15dc46a24285e296532d9704e22a18eb8c96e5
DrCookie <timothee.vanhove@heig-vd.ch>
2025-05-29 21:32:56 +0200

    init server

---
+++ polaris-server/src/main/kotlin/ch/heigvd/iict/ExampleResource.kt
@@ -0,0 +1,14 @@
+package ch.heigvd.iict
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+
+@Path("/")
+class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "Hello from POLARIS Server"
+}
